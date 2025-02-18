3fefa24bd6629e6b6900b1164d11415540c8fb1f
Adnan <adnans0307@gmail.com>
2025-02-18 16:57:07 +0530

    ok

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { configDotenv } from "dotenv";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBVdVeiZ5-UdFVgpMmqTlp6xCYQE1tSXIk",
-  authDomain: "chatroom-cafc5.firebaseapp.com",
-  projectId: "chatroom-cafc5",
-  storageBucket: "chatroom-cafc5.firebasestorage.app",
-  messagingSenderId: "629834151606",
-  appId: "1:629834151606:web:0519a7f2daff81a2969817"
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket ,
+  messagingSenderId: process.env.messagingSenderId ,
+  appId: process.env.appId 
 };
 
 const app = initializeApp(firebaseConfig);
